e252993303e5b3d6f0d42f58c3e4f0dac845b0f0
DoveCat <dove@jinzhijun.cn>
2022-04-26 09:47:29 +0800

    修改数据库名字

--- src/database/migrations/2018_02_05_023341_create_doveadmin_admins_table.php
+++ src/database/migrations/2018_02_05_023341_create_doveadmin_admins_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminsTable extends Migration
+class CreateBaseAdminsTable extends Migration
 {
     /**
      * Run the migrations.
--- src/database/migrations/2018_07_26_035645_create_doveadmin_admins_log_table.php
+++ src/database/migrations/2018_07_26_035645_create_doveadmin_admins_log_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminsLogTable extends Migration
+class CreatedBaseAdminsLogTable extends Migration
 {
     /**
      * Run the migrations.
